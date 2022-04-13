4d46ca1091de7eb4227a9cf4c561913cc668b26c
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-04-13 16:05:45 +0300

    add

--- 002-filtering-animation/src/App.js
+++ 002-filtering-animation/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import {useEffect, useState} from 'react';
 import Movie from './components/Movie';
+import Filter from './components/Filter';
+
 function App() {
   const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
 
   useEffect(() => {
     fetchPopular();
@@ -14,13 +18,20 @@ function App() {
     const movies = await data.json();
     console.log(movies); //for testing
     setPopular(movies.results);
+    setFiltered(movies.results);
   }
 
   return (
     <div className="App">
+    <Filter 
+    popular={popular}
+    setFiltered={setFiltered} 
+    activeGenre={activeGenre} 
+    setActiveGenre={setActiveGenre} />
+    {/*End of */}
       <div className='populr-movies'>
          {popular.map((movie) => {
-           return <Movie/>
+           return <Movie key={movie.id} movie={movie}/>
          })}
       </div>
     </div>
