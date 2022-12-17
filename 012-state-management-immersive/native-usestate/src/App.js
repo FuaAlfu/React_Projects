9142da308eaaea780872efd5b4a5bf7497fdb182
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-12-17 21:49:09 +0300

    add

--- 012-state-management-immersive/native-usestate/src/App.js
+++ 012-state-management-immersive/native-usestate/src/App.js
@@ -1,6 +1,33 @@
 import { useState } from 'react'
 
-function App() {
+function NameList() {
+    const [list, setList] = useState(["jhon", "Doe", "Fua","Juhha"]);
+    const [name, setName] = useState("");
+
+    function onAddName(){
+        list.push(name);
+        setList([...list,name]);
+        setName("");
+    }
+    return(
+    <div>
+       <ul>
+          {list.map((name) => (
+              <li key={name}>{name}</li>
+          ))}
+       </ul>
+    {/**/}
+    <input
+      type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    /> 
+    <button onClick={onAddName}>
+    </button>   
+    </div>
+    );
+}
+function Counter() {
     let [count, setCount] = useState(10);
 
     function addOne(){
@@ -11,6 +38,16 @@ function App() {
         <button onClick={addOne}>Count: {count}</button>
         </div>
     );
+}
+
+function App() {
+    return(
+        <div>
+           <Counter />
+           <Counter />
+           <NameList />
+        </div>
+    )
 }
 
 export default App;
---
+++ 013-nextjs-fullstackapp/README.md
@@ -0,0 +1,11 @@
+---
+stack: react js
+lang: all
+---
+
+# nextjs full stack app
+
+## create app
+```
+yarn create next-app
+```
