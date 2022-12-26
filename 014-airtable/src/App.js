76d33e1374d47f567fdac117933cddba099b14fa
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-12-26 22:51:36 +0300

    add

---
+++ 014-airtable/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import airtable from 'airtable';
+
+function App() {
+  const base = new Airtable({apikey:}).base('');
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ 014-airtable/src/components/Header.js
@@ -0,0 +1,11 @@
+
+import styled from 'styled-components';
+function Header() {
+    return (
+      <div className="title">
+         <h1>title</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
