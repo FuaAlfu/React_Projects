14b37d4b93e5791ee1797559d5258f66fcd5993b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-01-16 23:51:57 +0300

    add

--- 012-state-management-immersive/native-useMemo-useEffect-callBack/src/App.js
+++ 012-state-management-immersive/native-useMemo-useEffect-callBack/src/App.js
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
 
 function App() {
     const [count, setCount] = useState(0)
     const [numbers] = useState([10,100,1000,10000]);
-    const total = numbers.reduce((acc, number) => acc + number,0);
+    const total = useMemo(() => numbers.reduce((acc, number) => acc + number,0),[numbers]);
+    const [names,setNames] = useState(["n1","n2","n3"]);
+    {/*copy names before sorted */}
+    const sortedNames = useMemo(() =>  [...names].sort(),
+    [names]);
+
     return (
         <div className = "App" >
            Total: {total}
+           --
+           Names: {names.join(", ")}
+           --
+           Sorted Names: {sortedNames.join(", ")}
         </div>
     )
 }
