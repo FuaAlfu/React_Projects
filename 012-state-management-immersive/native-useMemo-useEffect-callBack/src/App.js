cb3adfa48700e46922477611dc96b2af0039b163
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-01-19 01:34:09 +0300

    add

--- 012-state-management-immersive/native-useMemo-useEffect-callBack/src/App.js
+++ 012-state-management-immersive/native-useMemo-useEffect-callBack/src/App.js
@@ -1,6 +1,13 @@
 import { useState, useMemo } from 'react'
 import './App.css'
 
+function SortedList({list}){
+    const sortedList = useMemo(() =>  [...list].sort(),[list]); 
+    return(
+        <div>
+        </div>
+    )
+}
 function App() {
     const [count, setCount] = useState(0)
     const [numbers] = useState([10,100,1000,10000]);
@@ -10,13 +17,23 @@ function App() {
     const sortedNames = useMemo(() =>  [...names].sort(),
     [names]);
 
+    const [count1,setCount1] = useState(0);
+    const [count2,setCount2] = useState(0);
+
+    {/*try: const countTotal = useMemo(() => count1 + count2, [count1, count2]);*/}
+    const countTotal = count1 + count2;
     return (
         <div className = "App" >
-           Total: {total}
+           <div>Total: {total}</div>
            --
-           Names: {names.join(", ")}
+           <div>Names: {names.join(", ")}</div>
+           <sortedNames list={names}/>
            --
-           Sorted Names: {sortedNames.join(", ")}
+           <div>Sorted Names: {sortedNames.join(", ")}</div>
+           --
+           <button onClick={() => setCount1(count1 + 1)}>Count: {count1}</button>
+           <button onClick={() => setCount2(count2 + 1)}>Count: {count2}</button>
+           <div>Total: {countTotal}</div>
         </div>
     )
 }
