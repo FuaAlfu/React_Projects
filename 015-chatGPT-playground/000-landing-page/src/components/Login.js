56d7013f03f0c4bea49f725f05112b3d64f36380
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-01-27 18:58:59 +0300

    add

---
+++ 015-chatGPT-playground/000-landing-page/src/components/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router';
+
+const Login = () =>{
+render(){
+return (
+<div id = "webapp">
+
+<h1>Welcome to YourWebApp!</h1> 
+
+<p id="LandingDescription"> 
+A custom website for your business that's easy to manage and maintain. 
+</p>
+
+<Link to="/CreateLogin"><button className="registerBtn">
+ Create an Account 
+ </button></Link>
+ {/* End of link */}
+<Link to="/ViewLogin"><button className="loginBtn"> Login </button></Link>
+
+</div>
+);
+}
+}
+export default Login;
---
+++ 015-chatGPT-playground/README.md
@@ -0,0 +1,11 @@
+---
+stack: react js
+lang: all
+---
+
+# things management in react
+
+## create app with vite using react template
+```
+yarn create vite app-name --template react
+```
