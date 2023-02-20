9a6b9d05e6ed274c5bd051b64a72f647295655f5
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-02-20 06:18:17 +0300

    add

--- 012-state-management-immersive/native-useMemo-useEffect-callBack/src/compenents/Testing.js
+++ 012-state-management-immersive/native-useMemo-useEffect-callBack/src/compenents/Testing.js
@@ -16,23 +16,39 @@ const Testing = () => {
       .then(data => setNames(data));
     },[]);
 
-    const [selectedNames,setSelectedNames] = useState(null);
     const [selectNameDetails, setSelectNameDetails] = useState(null);
 
-    useEffect(() => {
-        fetch(`/${selectedNames.json}`)
+    // const [selectedNames,setSelectedNames] = useState(null);
+    //     useEffect(() => {
+    //   if(selectedNames){
+    //     fetch(`/${selectedNames.json}`)
+    //       .then(respone => respone.json())
+    //       .then(data => setSelectNameDetails(data));
+    //     }
+    // },[selectedNames]);
+
+    const onSelectedNamesChange = (name) => {
+      fetch(`/${name}`)
           .then(respone => respone.json())
           .then(data => setSelectNameDetails(data));
-        },[selectedNames]);
+    };
 
     render(
         <div>
              <h1>testing</h1>
              Names: {names.join(", ")}
              ---
+             {/*
              <div>
              {names.map((name) => <button 
                 onClick={() => setSelectedNames(name)}>
+                {name}
+                </button>)}
+             </div>
+             */}
+             <div>
+             {names.map((name) => <button 
+                onClick={() => onSelectedNamesChange(name)}>
                 {name}
                 </button>)}
              </div>
